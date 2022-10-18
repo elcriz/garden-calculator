c0f2b59d51751922763dc2ed756ec9ac1e8ede84
Christiaan Hemerik <Christiaan.Hemerik@thelearningnetwork.nl>
2022-10-18 11:27:39 +0200

    Adds first files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import classNames from 'classnames';
+import { format } from 'date-fns';
+import plantsService from './services/plantsService';
 
-function App() {
+const months = {
+  1: 'Januari',
+  2: 'Februari',
+  3: 'Maart',
+  4: 'April',
+  5: 'Mei',
+  6: 'Juni',
+  7: 'Juli',
+  8: 'Augustus',
+  9: 'September',
+  10: 'Oktober',
+  11: 'November',
+  12: 'December',
+};
+
+const App = () => {
+  const [currentMonth, setCurrentMonth] = useState(
+    Number(format(new Date(), 'M'))
+  );
+  const [plants, setPlants] = useState([]);
+  const [error, setError] = useState('');
+  const [isFetching, setIsFetching] = useState(true);
+
+  useEffect(() => {
+    const fetchPlants = () => {
+      setError('');
+      plantsService.retrieveMonth(currentMonth)
+        .then((items) => {
+          setPlants(items);
+        })
+        .catch(({ message }) => {
+          setError(message);
+        })
+        .finally(() => {
+          setIsFetching(false);
+        })
+    };
+
+    fetchPlants();
+  }, [currentMonth]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={classNames('app', {
+      'is-loading': isFetching,
+    })}>
+      <header>
+        <h1>Garden Calendar</h1>
       </header>
+      <nav>
+        <ul className="months">
+          {Object.keys(months).map(month => (
+            <li
+              key={month}
+              className={classNames('month', {
+                'is-current': month === currentMonth,
+              })}
+            >
+              <button
+                type="button"
+                onClick={() => {
+                  setCurrentMonth(month)
+                }}
+              >
+                {months[month]}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <main>
+        <ul className="plants">
+          {!isFetching && plants.map(({ name, tasks }, taskIndex) => (
+            <li
+              key={taskIndex}
+              className="plant"
+            >
+              <h2>{name}</h2>
+              <ul className="tasks">
+                {tasks.map(({ title, description, infoUrl }, taskIndex) => (
+                  <li
+                    key={taskIndex}
+                    className="task"
+                  >
+                    <h3>{title}</h3>
+                    {description && (
+                      <div className="task__description">
+                        {description}
+                      </div>
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+        {error && (
+          <div className="error">
+            {error}
+          </div>
+        )}
+      </main>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/services/plantsService.js
@@ -0,0 +1,27 @@
+const plantsService = {
+  retrieveMonth: async (month) => {
+    try {
+      const response = await fetch('../data/plants.json');
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      const plants = await response.json();
+
+      const items = plants.reduce((previous, plant) => {
+        const tasks = plant.tasks.filter(task => task.months.indexOf(month) !== -1);
+        if (tasks.length === 0) {
+          return previous;
+        }
+        return [...previous, {
+          ...plant, tasks,
+        }];
+      }, []);
+
+      return items;
+    } catch (error) {
+      throw error;
+    }
+  }
+};
+
+export default plantsService;
