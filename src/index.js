705b01fbd23febf2f4dd296d9c6bfdc45bda5e2c
GAME-PC-CHRISTI\Christiaan Hemerik <elcriz@gmail.com>
2022-10-25 16:09:48 +0200

    WIP Github pages

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 import './css/main.css';
@@ -7,6 +8,8 @@ import './css/main.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
